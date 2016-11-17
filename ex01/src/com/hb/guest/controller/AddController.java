c94c81d415a82849a581e178b914a1ac4a667432
Gi-Yeong <kiyoung85@gmail.com>
2016-11-17 09:18:27 +0900

    immplement web.xml

--- ex01/src/com/hb/guest/controller/AddController.java
+++ ex01/src/com/hb/guest/controller/AddController.java
@@ -1,23 +1,21 @@
 package com.hb.guest.controller;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.hb.guest.model.GuestDao;
 import com.hb.guest.model.GuestDto;
 
-@WebServlet("/guest/add.do")
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet("/guest/add.do")
 public class AddController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("WEB-INF/guest/add.jsp").forward(request, response);
 	}
-	@Override
+
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		GuestDao dao = null;
